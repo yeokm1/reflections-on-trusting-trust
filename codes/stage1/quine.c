7eccb0a556d07aacef4bd018b0cab92471b69baa
Yeo Kheng Meng <yeokm1@gmail.com>
2015-12-12 18:27:10 +0800

    quine update to have space after stdio

--- codes/stage1/quine.c
+++ codes/stage1/quine.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-#define TEXT "#include <stdio.h>%c#define TEXT %c%s%c%c%cint main(){%c   //Prints own source code and injects newlines(10) and apostrophes(34)%c   printf(TEXT, 10, 34, TEXT, 34, 10, 10, 10, 10, 10, 10, 10);%c   return 0;%c}%c"
+
+#define TEXT "#include <stdio.h>%c%c#define TEXT %c%s%c%c%cint main(){%c   //Prints own source code and injects newlines(10) and apostrophes(34)%c   printf(TEXT, 10, 10, 34, TEXT, 34, 10, 10, 10, 10, 10, 10, 10);%c   return 0;%c}%c"
 
 int main(){
    //Prints own source code and injects newlines(10) and apostrophes(34)
-   printf(TEXT, 10, 34, TEXT, 34, 10, 10, 10, 10, 10, 10, 10);
+   printf(TEXT, 10, 10, 34, TEXT, 34, 10, 10, 10, 10, 10, 10, 10);
    return 0;
 }
