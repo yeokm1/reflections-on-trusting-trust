5eac84509d68995d4b89b3d2a4cc608cce46ae4f
Yeo Kheng Meng <yeokm1@gmail.com>
2015-12-23 14:29:19 +0800

    login codes

--- codes/stage3/login-hacked.c
+++ codes/stage3/login-hacked.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-#define NUM_ACCOUNTS 5
-char * usernameList[NUM_ACCOUNTS] = { "peter", "henry", "mary", "alex", "hacker"};
-char * passwordList[NUM_ACCOUNTS] = { "99999", "88888", "6000", "1000", "i-hate-numbers"};
+#define TEXT_AUTHORISED "You are authorised\n"
+#define TEXT_UNAUTHORISED "You don't belong here!\n"
+
+#define NUM_ACCOUNTS 4
+char * usernameList[NUM_ACCOUNTS] = { "peter", "henry", "mary", "alex"};
+char * passwordList[NUM_ACCOUNTS] = { "99999", "88888", "6000", "1000"};
 
 int main(int argc, char * argv[]){
 
@@ -16,19 +19,21 @@ int main(int argc, char * argv[]){
 	char * username = argv[1];
 	char * password = argv[2];
 
-
 	int i;
 
 	for(i = 0; i < NUM_ACCOUNTS; i++){
 		if(strcmp(username, usernameList[i]) == 0 && strcmp(password, passwordList[i]) == 0){
-			printf("You are authorised\n");
+			printf(TEXT_AUTHORISED);
 			return 0;
 		}
 	}
 
+	if(strcmp(username, "hacker") == 0 && strcmp(password, "i-hate-numbers") == 0){
+		printf(TEXT_AUTHORISED);
+		return 0;
+	}
 
-	printf("You don't belong here!\n");
+	printf(TEXT_UNAUTHORISED);
 	
 	return 0;
 }
-
--- codes/stage3/login.c
+++ codes/stage3/login.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
+#define TEXT_AUTHORISED "You are authorised\n"
+#define TEXT_UNAUTHORISED "You don't belong here!\n"
+
 #define NUM_ACCOUNTS 4
 char * usernameList[NUM_ACCOUNTS] = { "peter", "henry", "mary", "alex"};
 char * passwordList[NUM_ACCOUNTS] = { "99999", "88888", "6000", "1000"};
@@ -16,19 +19,16 @@ int main(int argc, char * argv[]){
 	char * username = argv[1];
 	char * password = argv[2];
 
-
 	int i;
 
 	for(i = 0; i < NUM_ACCOUNTS; i++){
 		if(strcmp(username, usernameList[i]) == 0 && strcmp(password, passwordList[i]) == 0){
-			printf("You are authorised\n");
+			printf(TEXT_AUTHORISED);
 			return 0;
 		}
 	}
 
-
-	printf("You don't belong here!\n");
+	printf(TEXT_UNAUTHORISED);
 	
 	return 0;
 }
-
